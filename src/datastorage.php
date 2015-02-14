0588a14ede89ff242b2daac167c4799df4174fdd
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-14 08:33:16 -0600

    datastorage accessing db fine

--- src/datastorage.php
+++ src/datastorage.php
@@ -1,13 +1,11 @@
 <?php 
 
-ini_set('display errors', 'On');
-include 'storedInfo.php';
+ini_set('display_errors', 'On');
 
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "wrighste-db", "ydEX8evfxykcq1xZ","wrighste-db");
-if ($mysqli->connect_errono) {
-	echo "Failed to connect MYSQL: (" .$mysqli->connect_errno. ")" . .$mysqli->connect_error;
-
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "wrighste-db", "ydEX8evfxykcq1xZ" ,"wrighste-db");
+if ($mysqli->connect_errno) {
+	echo "Failed to connect MYSQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else {
-	echo "connection worked!! <br>";
+	echo "Connection worked!! <br>";
 }
 ?>
