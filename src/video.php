65c933871f4d3cd69364924efb7c1fa12f340d5b
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-14 22:38:22 -0600

    add beginning of ui

---
+++ src/video.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="datastorage4.php" method="post">
+
+Enter name of video <input type="text" name="name"></br>
+Enter name of category <input type="text" name="category"></br>
+Enter movie lenght in minutes <input type="number" name="length"></br>
+<button type="submit">Login</button></br>
+</body>
+</html> 
