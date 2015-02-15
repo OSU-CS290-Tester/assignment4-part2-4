d1949b27d3289a29ceaa4aecfa101f34226ab601
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-15 17:12:52 -0600

    ugly, but it displays list of videos..will delete the list...no individual line delete yet

--- src/video.php
+++ src/video.php
@@ -1,10 +1,11 @@
 <html>
 <body>
-<form action="datastorage4.php" method="post">
+<form action="datastorage6.php" method="post">
 
 Enter name of video <input type="text" name="name"></br>
 Enter name of category <input type="text" name="category"></br>
 Enter movie lenght in minutes <input type="number" name="length"></br>
 <button type="submit">Add Video</button></br>
+<button type="submit" name="deleteall">Delete All Videos</button></br>
 </body>
 </html> 
