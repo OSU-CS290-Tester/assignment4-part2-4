aa869c360e92a5e530972d912f73e6d672cef760
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-14 22:26:25 -0600

    got php working with isql...currently just sending over hard-coded stuff to database

---
+++ src/datastorage4.php
@@ -0,0 +1,26 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "wrighste-db", "ydEX8evfxykcq1xZ", "wrighste-db");
+if ($mysqli->connect_errno) {
+echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} else {
+	echo "Connection worked!! <br>";
+}
+/* Prepared statement, stage 1: prepare -->NAME */ 
+if (!($stmt = $mysqli->prepare("INSERT INTO webdev1(id,name,category,length,rented) VALUES (?,?,?,?,?)"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+$id = 1;
+$name = "Lost in translation";
+$category = "xyz";
+$length = 120;
+$rented = 1;
+
+$stmt->bind_param("issii", $id,$name,$category,$length,$rented);
+
+if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+?>
