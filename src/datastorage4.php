e18e7af245b98671bcc06a883d65afddbc548129
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-14 23:09:14 -0600

    remove ID from ui since i auto-increment the ID

--- src/datastorage4.php
+++ src/datastorage4.php
@@ -7,21 +7,27 @@ echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->
 } else {
 	echo "Connection worked!! <br>";
 }
-/* Prepared statement, stage 1: prepare -->NAME */ 
-if (!($stmt = $mysqli->prepare("INSERT INTO webdev1(id,name,category,length,rented) VALUES (?,?,?,?,?)"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	if(empty($_POST['name']))
+    {
+         echo "Please click the back button in your browser and enter a video name";
+    } else
+	
+	{
+	/* Prepared statement, stage 1: prepare -->NAME */ 
+	if (!($stmt = $mysqli->prepare("INSERT INTO webdev1(id,name,category,length,rented) VALUES (?,?,?,?,?)"))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+
+	$id = $_POST['ID'];
+	$name = $_POST['name'];
+	$category = $_POST['category'];
+	$length = $_POST['length'];
+	$rented = 1;
+
+	$stmt->bind_param("issii", $id,$name,$category,$length,$rented);
+
+	if (!$stmt->execute()) {	
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
 }
-
-$id = $_POST['ID'];
-$name = $_POST['name'];
-$category = $_POST['category'];
-$length = $_POST['length'];
-$rented = 1;
-
-$stmt->bind_param("issii", $id,$name,$category,$length,$rented);
-
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-
 ?>
--- src/video.php
+++ src/video.php
@@ -2,10 +2,9 @@
 <body>
 <form action="datastorage4.php" method="post">
 
-Enter video ID <input type="number" name="ID"></br>
 Enter name of video <input type="text" name="name"></br>
 Enter name of category <input type="text" name="category"></br>
 Enter movie lenght in minutes <input type="number" name="length"></br>
-<button type="submit">Login</button></br>
+<button type="submit">Add Video</button></br>
 </body>
 </html> 
