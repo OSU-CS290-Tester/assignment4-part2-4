7046d8e31a3bfaa45de5e43ebfaa77ec11a586d3
xtephenw <wrighste@onid.oregonstate.edu>
2015-02-14 23:29:48 -0600

    continuing to debug

--- src/datastorage4.php
+++ src/datastorage4.php
@@ -11,23 +11,22 @@ echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->
     {
          echo "Please click the back button in your browser and enter a video name";
     } else
-	
 	{
 	/* Prepared statement, stage 1: prepare -->NAME */ 
-	if (!($stmt = $mysqli->prepare("INSERT INTO webdev1(id,name,category,length,rented) VALUES (?,?,?,?,?)"))) {
+	if (!($stmt = $mysqli->prepare("INSERT INTO webdev1(name,category,length,rented) VALUES (?,?,?,?)"))) {
 		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 	}
 
-	$id = $_POST['ID'];
 	$name = $_POST['name'];
 	$category = $_POST['category'];
 	$length = $_POST['length'];
 	$rented = 1;
 
-	$stmt->bind_param("issii", $id,$name,$category,$length,$rented);
-
+	$stmt->bind_param("ssii",$name,$category,$length,$rented);
+	
 	if (!$stmt->execute()) {	
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		echo "Movie Name already Exist in Database!";
+		//echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 	}
 }
 ?>
